01325441112c58d4ae5f78e2485a69d3c490b2ed
Abhishek Sharma <abhishek.sharma_cs18@gla.ac.in>
2020-02-24 17:50:23 +0530

    method to delete node at particular position

--- src/definition/MyLinkedList.java
+++ src/definition/MyLinkedList.java
@@ -42,4 +42,25 @@ public class MyLinkedList<T extends  Comparable<T>> {
         setHead(getHead().getNext());
         return temp;
     }
+    //method to delete node at particular position
+    public Node<T> delete(int i) {
+        Node<T> deletedNode ;
+        Node<T> temp = getHead();
+        if (isEmpty()) {
+            System.out.println("Underflow");
+            return temp;
+        }
+        if (i == 1) {
+            deletedNode = getHead();
+            setHead(null);
+        } else {
+            for (int j = 1; j < i - 1; j++) {
+                temp = temp.getNext();
+            }
+            deletedNode = temp.getNext();
+            temp.setNext(temp.getNext().getNext());
+        }
+        return deletedNode;
+    }
+
 }
