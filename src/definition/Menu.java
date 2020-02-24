3149a6e2d9f1f598b2fcf36ff2df13dad7c0659a
Abhishek Sharma <abhishek.sharma_cs18@gla.ac.in>
2020-02-24 18:00:14 +0530

    method to view choice

--- src/definition/Menu.java
+++ src/definition/Menu.java
@@ -14,4 +14,14 @@ public class Menu {
         scan = new Scanner(System.in);
         linkedList = new MyLinkedList<>();
     }
+    public int showChoice() {
+        System.out.println("Welcome to Devesh's Contact List App\n" +
+                "Press 1 to add a new contact\n" +
+                "Press 2 to view all contacts\n" +
+                "Press 3 to search for a contact\n" +
+                "Press 4 to delete a contact\n" +           //method to view choice
+                "Press 5 to exit program");
+        return scan.nextInt();
+    }
+
 }
