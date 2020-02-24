8fb216823366215202506e1e4ac593d68193b902
Abhishek Sharma <abhishek.sharma_cs18@gla.ac.in>
2020-02-24 17:32:01 +0530

    Getter for data

--- src/Helper/Node.java
+++ src/Helper/Node.java
@@ -4,4 +4,8 @@ package Helper;
 public class Node<T> {
     private T data;         //Data of node
     private Node<T> next;   //Self referential data
+    //Getter for data
+    public T getData() {
+        return data;
+    }
 }
