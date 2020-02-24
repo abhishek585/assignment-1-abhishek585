d4c8fe98ac1bf3639c42514e5eb405d247a13595
Abhishek Sharma <abhishek.sharma_cs18@gla.ac.in>
2020-02-24 17:39:10 +0530

    Getter for head

--- src/Helper/Node.java
+++ src/Helper/Node.java
@@ -20,4 +20,11 @@ public class Node<T> {
     public void setNext(Node<T> next) {
         this.next = next;
     }
+    @Override
+    public String toString() {
+        return "Node{" +
+                "data=" + data +
+                '}';
+    }
 }
+
--- src/definition/MyLinkedList.java
+++ src/definition/MyLinkedList.java
@@ -1,4 +1,13 @@
 package definition;
 
-public class MyLinkedList {
+import Helper.Node;
+
+public class MyLinkedList<T extends  Comparable<T>> {
+    private Node<T> head;           //It is the beginning of linked list
+    private Node<T> iter;           //It is used to iterate the list
+
+    public Node<T> getHead() {
+        return head;
+    }        //getter for head
+
 }
