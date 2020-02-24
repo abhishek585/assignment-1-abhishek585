6550b056b45b897335095029b7f5d887403f20c3
Abhishek Sharma <abhishek.sharma_cs18@gla.ac.in>
2020-02-24 17:39:55 +0530

    Setter for head

--- src/definition/MyLinkedList.java
+++ src/definition/MyLinkedList.java
@@ -9,5 +9,9 @@ public class MyLinkedList<T extends  Comparable<T>> {
     public Node<T> getHead() {
         return head;
     }        //getter for head
+    public void setHead(Node<T> node) {
+        head = node;
+        iter = head;
+    }   //setter for head
 
 }
