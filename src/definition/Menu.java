bfbdcd932b3b34c74e9730c9f58c8d3f6af40d17
Abhishek Sharma <abhishek.sharma_cs18@gla.ac.in>
2020-02-24 17:59:03 +0530

    constructor

--- src/definition/Menu.java
+++ src/definition/Menu.java
@@ -1,4 +1,17 @@
 package definition;
 
+import Helper.Node;
+import Helper.person;
+
+import java.util.Scanner;
+
 public class Menu {
+    private MyLinkedList<person> linkedList;
+    private Scanner scan;
+
+
+    public Menu() {                     //constructor
+        scan = new Scanner(System.in);
+        linkedList = new MyLinkedList<>();
+    }
 }
