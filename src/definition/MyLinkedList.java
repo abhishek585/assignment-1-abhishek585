b1d443e00f113c5a549824d441382d22a62d2247
Abhishek Sharma <abhishek.sharma_cs18@gla.ac.in>
2020-02-24 17:45:32 +0530

    method to insert new node

--- src/definition/MyLinkedList.java
+++ src/definition/MyLinkedList.java
@@ -19,4 +19,16 @@ public class MyLinkedList<T extends  Comparable<T>> {
     public void setIter(Node<T> iter) {
         this.iter = iter;
     }  //Setter for iter
+    //method to insert new node
+    public void insert(Node<T> node) {
+        if (getHead() == null) {
+            setHead(node);
+        } else {
+            Node<T> temp = head;
+            while (temp.getNext() != null) {
+                temp = temp.getNext();
+            }
+            temp.setNext(node);
+        }
+    }
 }
